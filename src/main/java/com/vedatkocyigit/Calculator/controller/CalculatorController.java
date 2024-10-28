564b7efe5efe89f6e707d1e7d3e8d66ed8513cfa
Vedat KOÇYİĞİT <hesap.kocyigit@gmail.com>
2024-10-29 01:26:00 +0300

    çarpma uç noktası tanımlandı,dto istek katmanı yazıldı ve test edildi

---
+++ src/main/java/com/vedatkocyigit/Calculator/controller/CalculatorController.java
@@ -0,0 +1,24 @@
+package com.vedatkocyigit.Calculator.controller;
+
+import com.vedatkocyigit.Calculator.dto.MultiplyRequest;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/calculator")
+public class CalculatorController {
+
+    @GetMapping("/sum")
+    public Double sum(@RequestParam(required = true) Double value1,@RequestParam(required = true) Double value2){
+
+        return value1+value2;
+    }
+
+    @PostMapping("/multip")
+    public Double multip(@RequestBody MultiplyRequest request){
+
+        Double val1 = request.getValue1() != null ? request.getValue1() : 0.0;
+        Double val2 = request.getValue2() != null ? request.getValue2() : 0.0;
+
+        return val1 * val2;
+    }
+}
---
+++ src/main/java/com/vedatkocyigit/Calculator/repository/CalculatorRepository.java
@@ -0,0 +1,7 @@
+package com.vedatkocyigit.Calculator.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CalculatorRepository {
+}
